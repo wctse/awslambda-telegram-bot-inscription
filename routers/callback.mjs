c6c85c03fff6607a539de7f98e09d263570cad44
wctse <47619083+wctse@users.noreply.github.com>
2024-01-16 21:47:27 +0800

    refactor: change switch of callback into if-else for complex cases

--- routers/callback.mjs
+++ routers/callback.mjs
@@ -3,115 +3,112 @@ import { handleImportWalletStep1 } from "../handlers/importWallet.mjs";
 import { handleStart } from "../handlers/start.mjs";
 import { handleMainMenu } from "../handlers/mainMenu.mjs";
 import { handleMintStep1, handleMintStep2, handleMintStep5, handleMintRepeat } from "../handlers/mint.mjs";
-import { handleTransfer } from "../handlers/transfer.mjs";
+import { handleTransferStep1 } from "../handlers/transfer.mjs";
 import { handleViewWallet } from "../handlers/viewWallet.mjs";
 import { handleSettings, handleSettingsGas } from "../handlers/settings.mjs";
 
 import { editUserState } from "../helpers/dynamoDB.mjs";
 
 export async function callback_router(data, chatId, messageId) {
-    switch (data) {
-        // -- INITIALIZATION -- //
-        // Create wallet in start
-        case 'create_wallet':
-            await handleCreateWallet(chatId);
-            break;
+    // -- INITIALIZATION -- //
+    // Create wallet in start
+    if (data === 'create_wallet') {
+        await handleCreateWallet(chatId);
+    }
 
-        // Import wallet in start
-        case 'import_wallet':
-            await handleImportWalletStep1(chatId);
-            break;
+    // Import wallet in start
+    else if (data === 'import_wallet') {
+        await handleImportWalletStep1(chatId);
+    }
 
-        // Back to start in import wallet
-        case 'start':
-            await handleStart(chatId);
-            break;
-
-        // -- GENERIC -- //
-        // Main menu or back to main menu in multiple interfaces
-        case 'main_menu':
-            await handleMainMenu(chatId);
-            break;
-
-        // Cancel and main menu in multiple interfaces
-        case 'cancel_main_menu':
-            await handleMainMenu(chatId);
-            await editUserState(chatId, 'IDLE');
-            break;
+    // Back to start in import wallet
+    else if (data === 'start') {
+        await handleStart(chatId);
+    }
 
 
-        // -- MAIN MENU -- //
-        // Mint button
-        case 'mint':
-            await handleMintStep1(chatId);
-            break;
+    // -- GENERIC -- //
+    // Main menu or back to main menu in multiple interfaces
+    else if (data === 'main_menu') {
+        await handleMainMenu(chatId);
+    }
 
-        // Transfer button
-        case 'transfer':
-            await handleTransfer(chatId);
-            break;
+    // Cancel and main menu in multiple interfaces
+    else if (data === 'cancel_main_menu') {
+        await editUserState(chatId, 'IDLE');
+        await handleMainMenu(chatId);
+    }
 
-        // View wallet button
-        case 'view_wallet':
-            await handleViewWallet(chatId);
-            break;
+    // -- MAIN MENU -- //
+    // Mint button
+    else if (data === 'mint') {
+        await handleMintStep1(chatId);
+    }
 
-        // Settings button
-        case 'settings':
-            await handleSettings(chatId);
-            break;
+    // Transfer button
+    else if (data === 'transfer') {
+        await handleTransferStep1(chatId);
+    }
 
-        // Refresh button
-        case 'refresh_main_menu':
-            await handleMainMenu(chatId);
-            break;
+    // View wallet button
+    else if (data === 'view_wallet') {
+        await handleViewWallet(chatId);
+    }
 
+    // Settings button
+    else if (data === 'settings') {
+        await handleSettings(chatId);
+    }
 
-        // -- MINT -- //
-        // ierc20 in mint step 1
-        case 'mint_step1_ierc20':
-            await handleMintStep2(chatId, 'ierc-20');
-            break;
-
-        // Confirm in mint step 4
-        case 'mint_step4_confirm':
-            await handleMintStep5(chatId);
-            break;
-
-        // Repeat in mint step 5
-        case 'mint_repeat':
-            await handleMintRepeat(chatId);
-            break;
-
-        
-        // -- VIEW WALLET -- //
-        // Refresh view wallet in view wallet
-        case 'refresh_view_wallet':
-            await handleViewWallet(chatId);
-            break;
-
-        
-        // -- SETTINGS -- //
-        case 'settings_gas_auto':
-            await handleSettingsGas(chatId, messageId, 'auto');
-            break;
-
-        case 'settings_gas_low':
-            await handleSettingsGas(chatId, messageId, 'low');
-            break;
-
-        case 'settings_gas_medium':
-            await handleSettingsGas(chatId, messageId, 'medium');
-            break;
-
-        case 'settings_gas_high':
-            await handleSettingsGas(chatId, messageId, 'high');
-            break;
+    // Refresh button
+    else if (data === 'refresh_main_menu') {
+        await handleMainMenu(chatId);
+    }
 
 
-        default:
-            console.info('Unknown callback query received:', data);
-            break;
+    // -- MINT -- //
+    // ierc20 in mint step 1
+    else if (data === 'mint_step1_ierc20') {
+        await handleMintStep2(chatId, 'ierc-20');
+    }
 
+    // Confirm in mint step 4
+    else if (data === 'mint_step4_confirm') {
+        await handleMintStep5(chatId);
+    }
+
+    // Repeat in mint step 5
+    else if (data === 'mint_repeat') {
+        await handleMintRepeat(chatId);
+    }
+
+
+    // -- VIEW WALLET -- //
+    // Refresh view wallet in view wallet
+    else if (data === 'refresh_view_wallet') {
+        await handleViewWallet(chatId);
+    }
+
+
+    // -- SETTINGS -- //
+    else if (data === 'settings_gas_auto') {
+        await handleSettingsGas(chatId, messageId, 'auto');
+    }
+
+    else if (data === 'settings_gas_low') {
+        await handleSettingsGas(chatId, messageId, 'low');
+    }
+
+    else if (data === 'settings_gas_medium') {
+        await handleSettingsGas(chatId, messageId, 'medium');
+    }
+
+    else if (data === 'settings_gas_high') {
+        await handleSettingsGas(chatId, messageId, 'high');
+    }
+
+    // Unknown data
+    else {
+        console.info('Unknown callback query received:', data);
     }
 }
